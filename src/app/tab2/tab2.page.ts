578a665f2d1b3842c27d74fb0013e5e67d6e4fd7
felipeabs01 <felipe.abs01@gmail.com>
2025-03-27 18:44:36 -0300

    cambios

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -14,6 +14,9 @@ export class Tab2Page {
   jugadoresAlfa :any=[];
   jugadoresBeta :any=[];
 
+  jugadorACambiar1:any;
+  jugadorACambiar2:any;
+
   constructor(  public equiposService: EquiposService,) {
 
     equiposService.equipos$.subscribe(data=>{
@@ -28,6 +31,86 @@ export class Tab2Page {
       console.log(data);
       this.jugadoresBeta = data;
     })
+
+  }
+
+  cambiarJugador(jug:any){
+    console.log(jug);
+
+    if(!this.jugadorACambiar2 && this.jugadorACambiar1)
+    this.jugadorACambiar2 = jug;
+
+    if(!this.jugadorACambiar1)
+    this.jugadorACambiar1 = jug;
+
+
+    console.log(this.jugadorACambiar1);
+    console.log(this.jugadorACambiar2);
+
+    console.log(this.jugadoresAlfa);
+    console.log(this.jugadoresBeta);
+    jug.cambiarJugador = true;
+
+   
+    if(this.jugadorACambiar1 && this.jugadorACambiar2){
+
+      let nuevaLista:any = [];
+      this.jugadores.forEach((jug:any) => {
+        if(jug == this.jugadorACambiar1 || jug == this.jugadorACambiar2 ){
+
+          if(jug == this.jugadorACambiar1){
+            this.jugadores.forEach((ju:any) => {
+              if(ju == this.jugadorACambiar2){
+                nuevaLista.push(ju);
+              }
+            });
+          }
+
+          if(jug == this.jugadorACambiar2){
+            this.jugadores.forEach((ju:any) => {
+              if(ju == this.jugadorACambiar1){
+                nuevaLista.push(ju);
+              }
+            });
+          }
+
+
+        }else{
+          nuevaLista.push(jug);
+        }
+      });
+
+      
+
+      console.log(nuevaLista);
+      this.jugadores = nuevaLista;
+      this.jugadorACambiar1 = null;
+      this.jugadorACambiar2 = null;
+      this.jugadoresAlfa = [];
+      this.jugadoresBeta = [];
+      
+      let mitadCantidad = this.jugadores.length / 2;
+      console.log(mitadCantidad);
+      
+      let contador = 1;
+      this.jugadores.forEach((element:any) => {
+        element.cambiarJugador = false;
+        if(contador <=mitadCantidad){
+          this.jugadoresAlfa.push(element);
+        }else{
+          this.jugadoresBeta.push(element);
+
+        }
+         contador = contador + 1;
+
+      });
+
+      console.log(this.jugadoresAlfa);
+      console.log(this.jugadoresBeta);
+      
+
+    }
+    
 
   }
 
